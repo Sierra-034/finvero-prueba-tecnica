50be18f7e8035a9a1871a9c29fec015529d4725c
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-09-15 00:24:01 -0600

    Add order's schemas

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,5 +1,6 @@
+from typing import List, Optional
 from pydantic import (
-    BaseModel, EmailStr, StrictStr,
+    BaseModel, EmailStr, StrictStr, Field,
     SecretStr, NonNegativeFloat, NonNegativeInt
 )
 
@@ -8,7 +9,30 @@ class UsuarioSchema(BaseModel):
     nombre: StrictStr
     password: SecretStr
 
+
 class ProductoSchema(BaseModel):
     nombre_producto: StrictStr
     precio_unitario: NonNegativeFloat
-    cantidad: NonNegativeInt
+    cantidad: NonNegativeInt = 1
+
+
+class ProductoInOrdenSchema(ProductoSchema):
+    precio_unitario: Optional[NonNegativeFloat] = 0.0
+
+
+class OrdenSchema(BaseModel):
+    nombre_cliente: StrictStr
+    lista_productos: List[ProductoInOrdenSchema]
+
+
+class OrdenOutSchema(OrdenSchema):
+    numero_orden: int
+    lista_productos: List[ProductoInOrdenSchema] = None
+    precio_total: float
+
+
+class OrdenOutListSchema(BaseModel):
+    numero_orden: int
+    precio_total: float
+    nombre_cliente: str
+    lista_productos: List[ProductoSchema]
